c4fc35261c5c9366e6e353c6444d2389fba1a4eb
Anna Rose Fritz <annarosefritz@Annas-MacBook-Pro-2.local>
2020-06-09 15:37:30 -0500

    a few thoughts

---
+++ src/negotiation20.v
@@ -0,0 +1,52 @@
+(*************
+
+Implementation of our work surrounding the concept of negotiation
+
+Anna Fritz and Perry Alexander 
+
+***************)
+
+(*******************
+
+Appraiser: goal is to verify the target is not compromised and can perform attestation to be deemed trustworthy 
+
+Target: body that is appraised to determine trustworthiness 
+
+Negotiation: the communication between the appraiser and target to determine the best protocol for attestation 
+
+Privacy Policy: the policy that ensure the target or appraiser does not share sensitive information 
+
+Selection Policy: a relation that maps concrete goals to abstract implementation
+
+
+ ********************)
+
+(*** 
+
+could outline the proof in section? With hypothesis? 
+
+dont we need the TRC? Saying that if we can get to one object we can get to others? 
+To help with the lattice ordering? 
+
+do we need some inductuve structure to describe the evidence? 
+
+ ***)
+
+Definition place := nat.
+
+Inductive term : Set :=
+| KIM : nat -> term
+| USM : nat -> term
+| AT : place -> term -> term
+| SEQ : term -> term -> term
+| PAR : term -> term -> term
+| SIG : term -> term.
+
+Definition request := term. 
+
+Definition proposal := term -> term. 
+
+Definition privacypolicy (ev : Type) := place -> term -> Prop. 
+
+Check privacypolicy. 
+
