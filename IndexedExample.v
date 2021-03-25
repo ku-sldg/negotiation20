c2655d0737cd94e8c33fa16a8507db127a230da9
Anna Fritz <42523108+arfritzz@users.noreply.github.com>
2021-03-25 18:03:28 -0500

    too many ways to do the same thing

--- IndexedExample.v
+++ IndexedExample.v
@@ -1,145 +1,99 @@
 Set Implicit Arguments.
+Require Import Coq.Lists.List.
+ 
+ (* 3 measurements 
+   1. VC
+   2. sign VC 
+   3. VC and SS in parallel *)
+ 
+ (* The place is where we want the measurement from *)
+ Inductive place : Type :=
+ | AP : place
+ | TP : place
+ | VC : place
+ | SS : place
+ | priv_key : place.
 
-Module Example1.
+ (* decidablility *)
+ Definition eq_place_dec: forall x y:place, {x=y}+{x<>y}.
+ Proof.
+ repeat decide equality.
+ Defined.
+ 
+  (* The class is if the information is sensitive. Red means sensitive *)
+ Inductive class : Type :=
+ | red : class
+ | green : class.
 
-(* let the appraiser be a place, the target be a place, 
-   ma be a trusted an isolated measurement and attestation 
-   domain, and q be a server that can respond to request *)
-Inductive place : Type :=
-| appraiser : place
-| target : place
-| ma : place
-| q : place.
-
-Inductive class : Type :=
-| red : class
-| green : class.
-
-Definition eq_class_dec: forall x y:class, {x=y}+{x<>y}.
-Proof.
+  (* decidablility *)
+  Definition eq_class_dec: forall x y:class, {x=y}+{x<>y}.
+  Proof.
   repeat decide equality.
-Defined.
+  Defined.
 
-(* evidence as a type *)
-Inductive evidence : Type :=
-| EBlob : class -> evidence
-| EHash : evidence
-| ECrypt : evidence -> place -> evidence
-| ESig : evidence -> place -> evidence
-| ESeq : evidence -> evidence -> evidence
-| EPar : evidence -> evidence -> evidence
-| EAt : place -> evidence -> evidence.
+ (* here, the structure of evidence is a Set 
+     https://ku-sldg.github.io/copland/resources/coplandcoq/html/Copland.Term.html *)
+ (* This structure has type: evidence _ where _ is the place where evidnece is gathered*)
+ Inductive evidence : place -> Type :=
+ | EBlob : forall p, class -> evidence p
+ | EHash : forall p, evidence p
+ | ECrypt : forall p q, evidence q -> place -> evidence p
+ | ESig : forall p q, evidence q -> place -> evidence p
+ | ESeq : forall p q r, evidence p -> evidence q -> evidence r
+ | EPar : forall p q r, evidence p -> evidence q -> evidence r.
 
-Definition eq_ev_dec: forall x y:evidence, {x=y}+{x<>y}.
-Proof.
-  repeat decide equality.
-Defined.
+ Definition good_encrypt := AP :: TP :: nil.
 
-(* terms indexed based on the evidence they produce *)
-Inductive term : evidence -> Type :=
-| TMeas : forall e, term e
-| THash : forall e, term e -> term EHash
-| TCrypt : forall e p, term e -> term (ECrypt e p)
-| TSig : forall e p, term e -> term (ESig e p)
-| TSeq : forall e f, term e -> term f -> term (ESeq e f)
-| TPar : forall e f, term e -> term f -> term (EPar e f)
-| TAt : forall e p, term e -> term (EAt p e).
+ Fixpoint privPolicy tp (e:evidence tp): Prop :=
+   match e with
+   | EHash _ => True
+   | EBlob p c => match p with 
+                  | SS => False 
+                  | _ => True 
+                  end
+   | ESig _ e' _ => privPolicy e' 
+   | ECrypt rp e' tp => if (in_dec (eq_place_dec) tp good_encrypt) then (match e' with 
+                                                                  | EBlob priv_key red => False 
+                                                                  | EBlob _ red => True 
+                                                                  | _ => privPolicy e'
+                                                                  end) 
+                                                                    else privPolicy e'
+   | ESeq _ l r => (privPolicy l) /\ (privPolicy r)
+   | EPar _ l r => (privPolicy l) /\ (privPolicy r)
+   end.
 
-Check TMeas (EBlob green).
+   Compute privPolicy (EBlob VC red).
+   Compute privPolicy (EBlob SS red).
 
-(* privacy policy *)
-
-(* Let's say the target does not wish to
-   share a measurement from place q 
-   with the appraiser. 
-   
-   This will be reflected in the EAt case *)
-Fixpoint privPolicyProp (ap : place) (e:evidence): Prop :=
-    match ap, e with
-    | _ , EHash => True
-    | _ , EBlob red => False
-    | _ , EBlob green => True
-    | _ , ECrypt e p =>  match ap with 
-                           | p => privPolicyProp ap e
-                           (* | _ => True *)
-                           end
-    | _ , ESig e' _ => privPolicyProp ap e'
-    | _ , ESeq l r => and (privPolicyProp ap l) (privPolicyProp ap r)
-    | _ , EPar l r => and (privPolicyProp ap l) (privPolicyProp ap r)
-    | _ , EAt tp e' => match ap, tp with 
-                      | appraiser, q => False 
-                      | _ , _ => privPolicyProp ap e' 
-                      end
-    end.
-
-    Compute TMeas (ECrypt (EBlob green) appraiser).
-
-    (* in this term, the appraiser is asking the target to 
-       encrypt a green blob with the target's public key *)
-    Compute privPolicyProp (appraiser) (ECrypt (EBlob green) target).
-    (* = True
-     : Prop *)
-
-     (* the appraiser asks the target to encrypt sensitive information 
-        with the appraiser's public key. This is bad! The appraiser 
-        could have access to the sensitive information. *)
-    Compute privPolicyProp (appraiser) (ECrypt (EBlob red) appraiser).
-    (* = False
-     : Prop*)
-
-    Definition privPolicyTProp p e (t:term e) := privPolicyProp p e.
-
-    (* This is our selection policy which creates a set of terms. 
-       It builds the set based on privacy policy satisfaction.   *)
-    Definition selectDep p e (_:term e) := {t:term e | privPolicyProp p e}.
-
-    Check selectDep. 
-    (* selectDep
-       : forall e : evidence, term e -> Set*) 
-    (* the return type of selectDep is a SET. *)
+ (* Checking to make sure the privacy policy is written correctly for ECrypt. 
+    The appraiser's place should should not match the place where encryption is 
+    occuring if the blob is red  *)
+ Compute privPolicy (EBlob priv_key red). 
+ Compute privPolicy (ECrypt TP (EBlob priv_key red) AP).
+ Compute privPolicy CC (ECrypt CC (EBlob CC red) AA).
+ Compute privPolicy CC (ECrypt AA (EBlob AA red) BB).
+ 
+ (* This measuement type checks but would be impossible to write
+    with the way the type system works.
     
-    (* The apprasier can ask for a measurement of a green blob*)
-    Compute selectDep appraiser (TMeas (EBlob green)).
-    (* = {_ : term (EBlob green) | True}
-    : Set *)
+    For some reason, I can't figure out how to get the crypt 
+    measurement to have place CC. It is always the 
+    same place as AA *)
+ Compute privPolicy CC (ECrypt CC (EBlob AA red) BB).
 
-    (* The appraiser can ask for a hash of a red blob *)
-    Compute selectDep appraiser (THash (TMeas (EBlob red))). 
-    (* 
-      = {_ : term EHash | True}
-     : Set *)
-
-    (* The appraiser can ask for a parallel measurement of the green blob and the hash *)
-    Compute selectDep appraiser (TPar (TMeas (EBlob green)) (THash (TMeas (EBlob red)))).
-    (* 	 = {_ : term (EPar (EBlob green) EHash) | True /\ True}
-         : Set *)
-
-    (* the appraiser CANNOT ask for a measurement from place q *)
-    Compute selectDep appraiser (TAt q (TMeas (EBlob green))).
-    (* 	 = {_ : term (EAt q (EBlob green)) | False}
-         : Set *)
-
-
-    Example selectDep1 : selectDep appraiser (TMeas (EBlob green)).
-    Proof. 
-      unfold selectDep. exists (TMeas (EBlob green)). reflexivity.
-    Qed.
-
-    Check selectDep1.
-
-    Check proj1_sig.
-    Check proj1_sig (selectDep1).
-    (* proj1_sig selectDep1
-	     : term (EBlob green)*)
-    Check proj2_sig (selectDep1).
-    (* proj2_sig selectDep1
-	     : privPolicyProp (EBlob green)*)
-    Compute proj1_sig (selectDep1). 
-    (* 	 = let (x, _) := selectDep1 in x
-         : term (EBlob green)*) 
-
-    (* No way to prove this so you can never get the value of the term. *)
-    Example selectDep2: selectDep appraiser (TAt q (TMeas (EBlob green))).
-    Proof. unfold selectDep. exists (TAt q (TMeas (EBlob green))). simpl. Abort.  
-
-End Example1.
+  (* Terms are indexed on the evidence they produce. First, they expect some measurement.
+     That is the first parameter that comes with `term e`. It may also expect the place that 
+     is asking for the measurment, ap.  
+     
+     -> privPolicy ap (EBlob p c)*)
+  Inductive term p : evidence p  -> Type :=
+  | TMeas : forall ap e,  privPolicy ap e -> term e
+  | THash : forall e, e -> term (EHash p)
+  | TSig :
+       forall ap e q, term e -> privPolicy ap (ESig p e q) -> term (ESig p e q)
+  | TCrypt :
+      forall (ap q : place) e , place -> term e -> privPolicy ap (ECrypt p e q) -> term (ECrypt p e q)
+  | TSeq : forall ap e f,
+      term e -> privPolicy ap e -> term f -> privPolicy ap f -> term (ESeq p e f)
+  | TPar : forall ap e f,
+      term e -> privPolicy ap e -> term f -> privPolicy ap f -> term (EPar p e f).
