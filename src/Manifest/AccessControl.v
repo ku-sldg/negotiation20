9964b544f2453a4468bd1dfc41c1225cdee14d05
Anna Fritz <42523108+arfritzz@users.noreply.github.com>
2022-08-25 11:30:31 -0500

    starting file for access control.

---
+++ src/Manifest/AccessControl.v
@@ -0,0 +1,8 @@
+(* Reasoning about Access Control. Motivated by paper titled "Model Checking Distributed Mandatory Access Control Policies" 
+
+Motivation : We need to be able to know if an AM can request measurement from another AM (measures relation). At the requested AM, we need to know if they can access a specific resource in the context of a certain appraiser. 
+
+Goal: create a type system to describe the target of measurement (objects) and the ASPs preforming measurement. 
+
+By: Anna Fritz (8.25.22) *)
+
