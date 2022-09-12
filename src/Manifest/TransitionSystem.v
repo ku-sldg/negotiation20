5f9926a6ed861c310e0e3f949f463b9cb105d99a
Anna Fritz <42523108+arfritzz@users.noreply.github.com>
2022-09-12 16:11:03 -0500

    example transition system

--- src/Manifest/TransitionSystem.v
+++ src/Manifest/TransitionSystem.v
@@ -2,71 +2,93 @@
 
 By: Anna Fritz (8.19.22) *)
 
-Require Import Manifest_map.
-Require Import Cop.Copland. 
+Require Import Lists.List.
+Import ListNotations.
+
+Require Import Cop.Copland.
 Import Copland.Term.
+Import Copland.Evidence. 
+Require Import Manifest. 
+Import Manifest.ManifestTerm. 
 
-Print Environment.
-Check Term. 
-Print Evidence. 
+Require Import String. 
 
-(***************************
-  DEFINING GENERATE FUNCTION 
-  *************************** *)
+(* TO DO check out Disclose.v & event semantics in term_defs.v *) 
 
-(* if we want to use the modeling check stuff then we can look here: https://github.com/achlipala/frap/blob/master/ModelChecking.v... but then we need a transition system  *)
-
-(* first, try writing "generate" as an imperative program. Start with k (the starting measurement place) gm (the global manifest) and l (an empty list of terms. this will be used as the return value )*)
-
-Print Manifest.
-(* Manifest = collection of ASPs and the measures relations *)
-
-Print System. 
-(* System = collection of manifests *)
-
-Print Environment.
-(* Environement = collection of systems. Will be useful for describing the *)
-
-(* For any place, "generate_ASPs" returns a list of ASPs present at that place. This is a first step at a more primitive way of generated a list of terms. *)
-Definition generate_ASPs (k: Plc) (s : System) : list ASP := 
-match s k with 
-| None => [] 
-| Some t => t.(lASP)
-end.  
-
-Print hasASP_dec.
-Print ASP.
-
-(* Process for generating protocols 
-    INPUT : system specifications, ideal piece of evidence 
-    OUTPUT : a list of evidence that meets specified requirements 
-    STEPS : 
-        1. Select with first piece of evidence in evidence chain
-        2. Check if corresponding ASP or measurement exists within system 
-        3. Repeat until all states have been visited or the result matches the ideal evidence *)
-
-(* QUESTIONS
-   - in the current evidence structure, there is no way to represent hashing siging or any of the ASP primitives besides ASPC ??
-    *)
-
-Print Evidence. 
-
-Fixpoint generate (s: System) (r : Evidence) (l : list Evidence): list Evidence := 
-    match r with 
-    | mt =>  l 
-    | nn id => [nn id] ++ l
-    | gg p a e => l
-    | _ => []
-    end. 
-
-(* here are the states. Either a final stat or an accumulation state. Eventually I think the final state will be a lattice of terms. *)
+(* Two states: 
+    1. answer state. 
+      This is the proposal. 
+    2. accumlator state. 
+      The input state will be the request while the accumulator 
+      state will begin as an empty list. Once the request is an
+      empty list, then the transition system moves into an 
+      answer state.  *)
 Inductive state := 
-| final_terms (term : list Term)
-| accumulate (input : Term) (acc : list Term).
+| proposal (pr : list Term)
+| requesting (input accumulator : list Term) (s:System).
 
-(* initalize with some original input added to an empty list. *)
-Inductive init (original_input : Term) : state -> Prop :=
-|Init : init original_input (accumulate (original_input) ([original_input] ++ [])).
+(* the initialization state starts with the request and an 
+   empty list *)
+Inductive init (req : list Term) (s: System) : state -> Prop := 
+| Init : init req s (requesting req [] s).
 
+(* the final state is the list of terms that are acceptable. AKA the proposal. *)
 Inductive final : state -> Prop := 
-| Final : forall t, final (final_terms t).
+| Final : forall prop, final (proposal (prop)).
+
+Definition check_exe (t:Term) (k:Plc) (s:System) : list Term := 
+  if executables_dec t k s then [t] else [].
+
+(* need to get the place of measurement to check executability *)
+Fixpoint get_Plc (t:Term) : Plc := 
+  match t with 
+  | asp (ASPC sp fwd (asp_paramsC id arg pl tar)) => pl 
+  | asp _ => "p"%string
+  | att p t => p 
+  | lseq t1 t2 => get_Plc t1 
+  | bseq sp t1 t2 => get_Plc t2 
+  | bpar sp t1 t2 => get_Plc t2
+  end.  
+
+Inductive step : state -> state -> Prop := 
+| Done : forall acc s, 
+          step (requesting [] acc s) (proposal acc)
+| Step : forall acc t s ts, 
+          step (requesting (t :: ts) acc s) (requesting ts (check_exe t (get_Plc t) s ++ acc) s).
+
+(* We need the TRC to reason about all states *)
+Inductive trc {A} (R : A -> A -> Prop) : A -> A -> Prop :=
+| TrcRefl : forall x, trc R x x
+| TrcFront : forall x y z,
+  R x y
+  -> trc R y z
+  -> trc R x z.
+
+  Notation "R ^*" := (trc R) (at level 0).
+
+  Definition example_sys_1 := env e3.
+
+  Print e0.
+  (* empty *)
+  
+  Print e1.
+  (* The relying party has ASPC1 (use asp1 to measure x and y) and can measure the target *) 
+
+  Print e2.
+  (* The target can sign the virus checker and request measurement from the appraiser *) 
+
+  Print e3. 
+  (* The apprsier can hash *)
+
+ (* Lets say the request is to hash the virus checker *)
+  Definition req1 := [asp (aspc2)].
+  
+  Example req_vc : step^* (requesting req1 [] example_sys_1) (proposal [asp (aspc2)]).
+  Proof.
+    simpl.
+    eapply TrcFront.
+    + apply Step.
+    + simpl. eapply TrcFront. simpl. apply Done. apply TrcRefl.
+  Qed. 
+
+
