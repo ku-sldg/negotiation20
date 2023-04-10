2d3e87acf221f021db3fcd95c56dae4c8f59a968
Anna Fritz <arfritzz@ku.edu>
2023-04-10 17:33:29 -0500

    still not working but for better reason

--- src/Refinement/ManifestExample.v
+++ src/Refinement/ManifestExample.v
@@ -1,6 +1,8 @@
 (* EXAMPLE SYSTEM 
 
-   an example system that has a virus checker that can be measured by the "attest" asp. 
+   an example system motivated by flexible mechanisms.
+   
+   example protocol is: @P1[(attest P1 sys) -> @P2[(appraise P2 sys) -> SIG
    
    By Anna Fritz (9.12.22)*)
 
@@ -15,33 +17,32 @@ Require Import Manifest.
 Import Manifest.Manifest.
 Import Manifest.ManifestProperties. 
 
+Require Import SA. 
+
 Require Import Utils.Utilities.
 Require Import String.
 
-  (** Proof tactic for proving [executable] given the above definition
-   *)
-   Ltac print_goal := match goal with |- ?A => idtac A end.
+(** Proof tactic for proving [executable] given the above definition *)
+Ltac print_goal := match goal with |- ?A => idtac A end.
 
-   Ltac prove_exec :=
-     simpl; auto; match goal with
-                  | |- hasASPe _ _ _ => cbv; left; reflexivity
-                  | |- knowsOfe _ _ _ => unfold knowsOfe; simpl; left; reflexivity
-                  | |- _ /\ _ => split; prove_exec
-                  | |- ?A => idtac A
-                  end.
- 
-   
- 
-   Ltac prove_execs :=
-     simpl; auto; match goal with
-                  | |- hasASPe _ _ _ => cbv; left; reflexivity
-                  | |- hasASPs _ _ _ => cbv; left; reflexivity
-                  | |- knowsOfe _ _ _ => unfold knowsOfe; simpl; left; reflexivity
-                  | |- knowsOfs _ _ _ => unfold knowsOfs,knowsOfe; simpl; left; reflexivity
-                  | |- _ /\ _ => split; prove_execs
-                  | |- _ \/ _ => try (left; prove_execs); try (right; prove_execs)
-                  | |- ?A => idtac A
-                  end.
+Ltac prove_exec :=
+  simpl; auto; match goal with
+              | |- hasASPe _ _ _ => cbv; left; reflexivity
+              | |- knowsOfe _ _ _ => unfold knowsOfe; simpl; left; reflexivity
+              | |- _ /\ _ => split; prove_exec
+              | |- ?A => idtac A
+              end.
+
+Ltac prove_execs :=
+  simpl; auto; match goal with
+              | |- hasASPe _ _ _ => cbv; left; reflexivity
+              | |- hasASPs _ _ _ => cbv; left; reflexivity
+              | |- knowsOfe _ _ _ => unfold knowsOfe; simpl; left; reflexivity
+              | |- knowsOfs _ _ _ => unfold knowsOfs,knowsOfe; simpl; left; reflexivity
+              | |- _ /\ _ => split; prove_execs
+              | |- _ \/ _ => try (left; prove_execs); try (right; prove_execs)
+              | |- ?A => idtac A
+              end.
 
 (* Motivated by the Flexible Mechanisms for Remote Attestation, 
     we have three present parties in this attestation scheme. 
@@ -49,7 +50,15 @@ Require Import String.
 Notation Rely := "Rely"%string.
 Notation Target := "Target"%string.
 Notation Appraise := "Appraise"%string.
-Notation sys := "System"%string. 
+Notation sys := "System"%string.
+
+Notation situation1 := Type. 
+
+(* Peers begin with ISAKMP to create security association *)
+Definition SA1 : SA := {| requestor := Rely ; 
+                          target := Target ; 
+                          situation := situation1 ; 
+                          lifetime := 12 |}.
 
 (* Measurement operations (ASPs of the form ASPID p t)
    * p is place where target t is located *)
@@ -66,31 +75,28 @@ Inductive rely_Policy : ASP -> Plc -> Prop :=
 
 (** target party can share attest with appraiser  *)
 Inductive tar_Policy : ASP -> Plc -> Prop := 
-| tar_pol : tar_Policy attest Appraise
-| tar_pol_sig : forall p, tar_Policy SIG p.
+| tar_pol : tar_Policy attest Appraise.
 
 (** appraiser can share result with relying party   *)
 Inductive app_Policy : ASP -> Plc -> Prop := 
-| app_pol : app_Policy app Rely. 
+| app_pol : app_Policy app Rely
+| app_pol_sig : forall p, app_Policy SIG p. 
 
-
-(** Definition of environments for use in examples and proofs.  Note there are 3 AM's present... Relying Party, Target, and Appraiser, each have one AM. 
-*)
+(** Definition of environments for use in examples and proofs.  Note there are 3 AM's present... Relying Party, Target, and Appraiser, each have one AM. *)
 Definition e0 := e_empty.
 Definition e_Rely :=
   e_update e_empty Rely (Some {| asps := [request]; M:= [Target] ; C := [] ; Policy := rely_Policy |}).
 Definition e_Targ :=
-  e_update e_empty Target (Some {| asps := [attest; SIG]; M:= [Appraise] ; C := [] ; Policy := tar_Policy|}).
+  e_update e_empty Target (Some {| asps := [attest]; M:= [Appraise] ; C := [] ; Policy := tar_Policy|}).
 Definition e_App :=
-  e_update e_empty Appraise (Some {| asps := [app] ; M:= [] ; C := [Target] ; Policy := app_Policy |}).
+  e_update e_Targ Appraise (Some {| asps := [app; SIG] ; M:= [] ; C := [Target] ; Policy := app_Policy |}).
 
-(* In our example, the system includes the relying party, the target, and the appraiser *)
-Definition example_sys_1 := [e_Rely; e_Targ; e_App]. 
+(* In our example, the system includes the relying party, the target *)
+Definition example_sys_1 := [e_Rely; e_App]. 
 
 (* example phrases *)
-Example request_attest := lseq (asp request) (att Target (lseq (asp attest) (asp SIG))).
-Example request_attest_appraise := lseq (request_attest) (att Appraise (asp app)).
-
+Example request_attest := lseq (asp request) (att Target (asp attest)).
+Example request_attest_appraise := lseq (request_attest) (att Appraise (lseq (asp app) (asp SIG))).
 
 (* Proofs about has ASP, depends On, and knows Of*)
 
@@ -110,8 +116,8 @@ Qed.
 (* within the system, we see that the appraiser depends on target *)
 Example ex4 : dependsOns Appraise example_sys_1 Target.
 Proof. 
-  unfold dependsOns. simpl. unfold dependsOne. simpl. auto.
-Qed.   
+  unfold dependsOns. simpl. eauto. unfold dependsOne. simpl. auto.
+Qed.  
 
 (* Proofs about executability *)
 
@@ -125,11 +131,11 @@ Proof.
   intros Hcontra.
   simpl in *.
   cbv in *.
-  destruct Hcontra.
+  destruct Hcontra; inversion H. 
 Qed.
 
-(* two signature operations are executable on the target*)
-Example ex7: (executable (lseq (asp SIG) (asp SIG)) Target e_Targ).
+(* two signature operations are executable on the appraiser*)
+Example ex7: (executable (lseq (asp SIG) (asp SIG)) Appraise e_App).
 Proof. prove_exec; unfold hasASPe; simpl; auto. Qed.
 
 (* executability of the request then attest *)
@@ -137,7 +143,7 @@ Example exe_1: (executables request_attest Rely example_sys_1).
     prove_exec.
     + unfold hasASPe. simpl. auto.
     + unfold knowsOfe. simpl. intros.
-      unfold hasASPe. simpl. split; auto.
+      unfold hasASPe. simpl. auto.
 Qed.
 
 (* executability of the whole phrase *)
@@ -145,35 +151,22 @@ Example exe_all : (executables request_attest_appraise Rely example_sys_1).
 Proof with simpl.
   prove_exec.
   + unfold hasASPe... auto.
-  + unfold knowsOfe... intros. unfold hasASPe... split; auto.
   + unfold knowsOfe... intros. unfold hasASPe... auto.
+  + unfold knowsOfe... intros. unfold hasASPe... split; auto.
 Qed.    
 
 (* Proofs about privacy policy . *)
 Check checkASPPolicy. 
 
+(*  The target should be able to share a measurement of attest with the apprasier.
 
-(*  The target should be able to share a measurement of attest with the apprasier. 
-    Here's the issue: we pass in the target and the target's environment 
-                      as well as the ASP in question... but we need to 
-                      specify WHO we are sharing this measurement with.... 
-                      we currently aren't doing that. *)
+    - Now the problem is the target shares the requested measurement with app 
+    - Need some idea of transition system to understand policy flow maybe???  
+    Should appraiser and target have a security associtation? 
+    I guess it is true that attest should not be shared with relying party.... 
+    maybe we need to add additional infrastruture like "covers" *)
 
-Example privPol1 : checkASPPolicy Target e_Targ attest.
+Example privPol1 : checkASPPolicy SA1 Target e_Targ attest.
 Proof.
-  unfold checkASPPolicy. simpl.  
-Abort. 
-
-(** Check environment [e] and see if place [p] has some policy 
-*  where the Policy allows p to run a. *)
-Definition checkASPPolicy' (rp:Plc) (p:Plc) (e:Environment) (a:ASP) :Prop :=
-  match (e p) with (* Look for p in the environment *)
-  | None => False
-  | Some m => (Policy m a rp) (* Policy from m allows rp to have measurement of a *)
-  end.
-
-(* Now proof finishes *)
-Example privPol1 : checkASPPolicy' Appraise Target e_Targ attest.
-Proof.
-  unfold checkASPPolicy'. simpl. apply tar_pol.  
-Qed.  
+  unfold checkASPPolicy. simpl.   
+Abort.   
