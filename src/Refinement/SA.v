dfa5ca7011f964464c1035258758738fa472d496
Anna Fritz <arfritzz@ku.edu>
2023-04-10 13:28:11 -0500

    Security Association

---
+++ src/Refinement/SA.v
@@ -0,0 +1,24 @@
+(* The security association is instantiated during ISAKMP.
+   It includes:  
+        - identites 
+        - keys 
+        - domain of interpretation
+        - situation
+        
+   For now, we only relize identities. *)
+Require Import Cop.Copland.
+Import Copland.Term.
+
+(* TO DO: define situation *)
+Definition sit : Type. Admitted. 
+
+Record SA := {
+    (* identity of the requestor *)
+    requestor : Plc ;
+    (* identity of the target... maybe this solves the "who answers the door"  *)
+    target : Plc;
+    (* situation field. TBD. *)
+    situation : sit ; 
+    (* lifetime of SA *)
+    lifetime : nat ;
+}. 
